d6cefc54cf0290108668d12824adb94b6d0e09ec
zerossa01 <madfoxsenpai@gmail.com>
2026-05-13 20:34:51 -0700

    Remove unused PrismaAdapter import

--- src/auth.ts
+++ src/auth.ts
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import NextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
-import { PrismaAdapter } from "@auth/prisma-adapter";
+
 import { prisma } from "@/lib/prisma";
 import bcrypt from "bcryptjs";
 
